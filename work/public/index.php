5f91484166f1f893e37a326c99bf3144a114db8f
kinutaISE <81940555+mKenta0325@users.noreply.github.com>
2022-06-14 15:23:20 +0900

    index.php の変更
    login_form.php を index.php に変更、元 index.php を削除

--- work/public/index.php
+++ work/public/index.php
@@ -8,7 +8,17 @@ include_once('_parts/_header.php') ;
 
 <body>
   <h1>将来設計</h1>
-  <p>登録済みの方は<a href="login_form.php">こちら</a></p>
+  <form method="post" action="login.php">
+    <div>
+      <label>ユーザーID</label>
+      <input type="text" name="user_id" minlength="5" maxlength="16" pattern="^[a-zA-Z0-9]+$" required>
+    </div>
+    <div>
+      <label>パスワード</label>
+      <input type="password" name="password" minlength="8" maxlength="20" pattern="^[a-zA-Z0-9]+$" required>
+    </div>
+    <button>ログイン</button>
+  </form>
   <p>未登録の方は<a href="signup.php">こちら</a></p>
 </body>
 
