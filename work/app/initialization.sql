aa93ca9cafd3295876001c50ad39ca68f669657c
kinutaISE <nknopt.ise@gmail.com>
2022-07-01 16:59:57 +0900

    年齢→生年月日

--- work/app/initialization.sql
+++ work/app/initialization.sql
@@ -12,9 +12,7 @@ CREATE TABLE users(
   id VARCHAR(255) NOT NULL, -- ユーザーID（主キー）
   password VARCHAR(255) NOT NULL, -- パスワード
   email VARCHAR(255) NOT NULL, -- メールアドレス
-  birth_year INT, -- 生年月日（年）
-  birth_month INT, -- 生年月日（月）
-  birth_date INT, -- 生年月日（日）
+  date_of_birth VARCHAR(255), -- 生年月日
   business_type_id VARCHAR(255), -- 事業種ID
   prefecture_id VARCHAR(255), -- 都道府県ID
   dependents_num INT, -- 扶養人数
