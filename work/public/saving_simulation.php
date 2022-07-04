7806bf22ef4280872a358aecb8ce772388bae83a
nakanok <94549186+kinutaISE@users.noreply.github.com>
2022-07-04 17:43:58 +0900

    年別収入額の計算（就労所得のみ）

--- work/public/saving_simulation.php
+++ work/public/saving_simulation.php
@@ -6,16 +6,27 @@ include_once('_parts/_header.php') ;
 
 $pdo = Database::getInstance() ;
 
+$incomes = IncomeSimulator::get_incomes($pdo) ;
+
 $costs = SavingSimulator::get_costs($pdo) ;
 
 ?>
 
 <body>
+  年別支出額：
   <ul>
     <?php foreach ($costs as $year => $cost):?>
       <li><?= $year . ':' . number_format($cost) . '円' ;?></li>
     <?php endforeach ; ?>
   </ul>
+  年別収入額：
+  <ul>
+    <?php foreach ($incomes as $year => $income):?>
+      <li><?= $year . ':' . number_format($income) . '円' ;?></li>
+    <?php endforeach ; ?>
+  </ul>
+  年別貯蓄額：
+  
   <p><a href="mypage.php">戻る</a></p>
 </body>
 
