36a025502ee8459c04bb9519f88dd5b47cd3eeca
nakanok <94549186+kinutaISE@users.noreply.github.com>
2022-07-04 17:57:09 +0900

    年別貯蓄額の計算

--- work/public/saving_simulation.php
+++ work/public/saving_simulation.php
@@ -7,8 +7,8 @@ include_once('_parts/_header.php') ;
 $pdo = Database::getInstance() ;
 
 $incomes = IncomeSimulator::get_incomes($pdo) ;
-
 $costs = SavingSimulator::get_costs($pdo) ;
+$savings = SavingSimulator::get_savings($pdo) ;
 
 ?>
 
@@ -26,7 +26,11 @@ $costs = SavingSimulator::get_costs($pdo) ;
     <?php endforeach ; ?>
   </ul>
   年別貯蓄額：
-  
+  <ul>
+    <?php foreach ($savings as $year => $saving):?>
+      <li><?= $year . ':' . number_format($saving) . '円' ;?></li>
+    <?php endforeach ; ?>
+  </ul>
   <p><a href="mypage.php">戻る</a></p>
 </body>
 
